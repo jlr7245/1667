45ad10463c6d28128ec1f7db877e93ff356b82f7
J Silverstein <jess.pebble@gmail.com>
2020-10-23 23:48:03 -0400

    scaffold

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+
+const StatusError = require('./utils/StatusError');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.locals.data = {};
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("this is not the endpoint yr looking for");
+});
+
+app.use("*", (req, res) => {
+  throw new StatusError(404, "Not found");
+});
+
+app.use((err, req, res, next) => {
+  res
+    .status(err.statusCode || 500)
+    .json({ message: err.message, stack: err.stack });
+});
