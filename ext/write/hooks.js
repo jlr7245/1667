ae472176ea62635c60d8e23e0793f2dfa58ea22f
J Silverstein <jess.pebble@gmail.com>
2020-10-31 21:15:44 -0400

    aaaaaaaaaaa

---
+++ ext/write/hooks.js
@@ -0,0 +1,7 @@
+const registerNewUser = (user) => {
+  // send some sort of message, return some token or other
+}
+
+const loginUser = (user) => {
+  // send some sort of message, return some token or other
+}
--- ext/write/main.js
+++ ext/write/main.js
@@ -46,6 +46,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const gear = document.querySelector('.fa-gear');
   const settings = document.querySelector('.settings');
   const themeButtons = document.querySelectorAll('.btn-group button');
+  const loginForm = document.querySelector('.login-or-register');
+  const loginModalContainer = document.querySelector('.login');
   const root = document.documentElement;
   let saveInterval;
 
@@ -79,13 +81,13 @@ document.addEventListener('DOMContentLoaded', () => {
       root.style.setProperty(fontVar, fontLookup[value]);
     }
   };
-  themeButtons.forEach(button => {
-    button.addEventListener('click', evt => {
+  themeButtons.forEach((button) => {
+    button.addEventListener('click', (evt) => {
       const { alter, value } = evt.target.dataset;
       console.log(alter, value);
       adjustTheme(alter, value);
-    })
-  })
+    });
+  });
 
   // handles showing / hiding the settings
   gear.addEventListener('click', () => {
@@ -112,5 +114,24 @@ document.addEventListener('DOMContentLoaded', () => {
       clearInterval(saveInterval);
       changesSinceLastSave = false;
     }
+  });
+
+  // handles form registration / display
+  loginForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    const username = evt.target.username.value;
+    const password = evt.target.password.value;
+    const isNewUser = evt.target.isnewuser.checked;
+    if (isNewUser) {
+      registerUser({ username, password }).then((result) => {
+        console.log('welcome to the result');
+        loginModalContainer.classList.add('hide');
+      });
+    } else
+      loginUser({ username, password }).then((result) => {
+        console.log('this sure is a result of some kind');
+        loginModalContainer.classList.add('hide');
+      });
+    console.log(evt.target.isnewuser.checked);
   });
 });
