9ea1b3ba96c08ca1c989b5714f578fa06e3bddbe
J Silverstein <jess.pebble@gmail.com>
2020-10-31 20:04:15 -0400

    theme switching

--- ext/write/main.js
+++ ext/write/main.js
@@ -3,6 +3,34 @@ console.log('hello world!');
 let hasBeenSaved = false;
 let changesSinceLastSave = false;
 
+const themeLookup = {
+  regular: ['#f7edb2', '#001628', '#001628', '#001628'],
+  regularDark: ['#001628', '#f7edb2', '#f7edb2', '#ffffff'],
+  darkroom: ['black', 'darkred', 'darkred', 'red'],
+  migraine: ['#00ff00', '#222', '#222', '#333'],
+  migraineDark: ['#222', '#00ff00', '#00ff00', '#00ff33'],
+};
+const colorVars = [
+  '--theme-background',
+  '--theme-text',
+  '--theme-border',
+  '--theme-button-hover',
+];
+
+const sizeVar = '--size';
+const sizeLookup = {
+  small: '18px',
+  medium: '22px',
+  large: '28px',
+};
+
+const fontVar = '--font';
+const fontLookup = {
+  sans: "'IBM Plex Sans', sans-serif",
+  serif: "'IBM Plex Serif', Georgia, Cambria, Cochin, sans-serif",
+  mono: "'IBM Plex Mono', monospace",
+};
+
 const splitter = (text) => {
   const byNewline = text.trim().split('\n');
   return byNewline.reduce((acc, val) => {
@@ -15,6 +43,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const writeBox = document.querySelector('.writebox');
   const count = document.querySelector('.count');
   const saveButton = document.querySelector('#save');
+  const gear = document.querySelector('.fa-gear');
+  const settings = document.querySelector('.settings');
+  const themeButtons = document.querySelectorAll('.btn-group button');
+  const root = document.documentElement;
   let saveInterval;
 
   saveButton.addEventListener('click', (evt) =>
@@ -34,9 +66,34 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('the text has been loaded');
   };
 
+  // adjusts the theme of the thing
+  const adjustTheme = (alter, value) => {
+    if (alter === 'theme') {
+      colorVars.forEach((color, idx) => {
+        root.style.setProperty(color, themeLookup[value][idx]);
+      });
+    } else if (alter === 'size') {
+      console.log(sizeVar, sizeLookup[value]);
+      root.style.setProperty(sizeVar, sizeLookup[value]);
+    } else if (alter === 'font') {
+      root.style.setProperty(fontVar, fontLookup[value]);
+    }
+  };
+  themeButtons.forEach(button => {
+    button.addEventListener('click', evt => {
+      const { alter, value } = evt.target.dataset;
+      console.log(alter, value);
+      adjustTheme(alter, value);
+    })
+  })
+
+  // handles showing / hiding the settings
+  gear.addEventListener('click', () => {
+    settings.classList.toggle('hide');
+  });
+
   // handles input on the box
   writeBox.addEventListener('input', (evt) => {
-    console.log('AN EVENT HAPPENED')
     changesSinceLastSave = true;
     const wordList = splitter(evt.target.innerText);
     count.innerText = wordList.length;
@@ -46,7 +103,7 @@ document.addEventListener('DOMContentLoaded', () => {
         save(JSON.stringify(evt.target.innerHTML));
         saveInterval = setInterval(() => {
           if (changesSinceLastSave) {
-            console.log(changesSinceLastSave)
+            console.log(changesSinceLastSave);
             saveButton.removeAttribute('disabled');
           }
         }, 5000);
